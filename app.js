8039a47dececd4cd2f75ca2c4366cc753f1654a1
Fereshteh Heidari <fereshte121@gmail.com>
2021-01-31 18:18:05 -0800

    using Mongoose to add database

--- app.js
+++ app.js
@@ -25,24 +25,58 @@
 //   }
 // }
 // run().catch(console.dir);
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
+const Fruit = mongoose.model("Fruit", fruitSchema);
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "pretty solid!"
+});
+// fruit.save()
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+})
+const People = mongoose.model("People", personSchema);
+const people = new People({
+  name: "John",
+  age: 37
+});
+// people.save();
+const kiwi = new Fruit({
+  name: "kiwi",
+  rating: 7,
+  review: "excellent!"
+})
+const banana = new Fruit({
+  name: "banana",
+  rating: 5,
+  review: "decent"
+})
+// Fruit.insertMany([kiwi,banana],function(err){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log("Successfully saved all the fruits")
+//   }
+// });
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log("error!");
+  } else {
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    })
+  }
 
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url,{ useUnifiedTopology: true } );
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
 });
